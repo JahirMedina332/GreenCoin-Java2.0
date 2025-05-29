662ad9eba2fae966e6e2e9097e3abbc590c5350b
JahirMedina332 <antonioloor558@gmail.com>
2025-05-28 22:56:10 -0500

    Revert "Merge branch 'main' of https://github.com/Sebasthein/AppReciclaje"
    This reverts commit 7e695fb2c923b534d85a9ed0c53e0eb25387c954, reversing
    changes made to 1ea3ce39a1e3947f4ae30ff2a4748f6a71440c65.

--- reciclaje/src/main/java/com/example/reciclaje/entidades/Usuario.java
+++ reciclaje/src/main/java/com/example/reciclaje/entidades/Usuario.java
@@ -10,8 +10,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -24,14 +22,11 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.PostLoad;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "usuarios")
@@ -53,17 +48,7 @@ public class Usuario implements UserDetails {
     private String password;
     private int puntos;
     
-    // Método para actualizar los puntos
-    public void actualizarPuntos() {
-        this.puntos = this.reciclajes.stream()
-                .mapToInt(Reciclaje::getPuntosGanados)
-                .sum();
-    }
     
-    // Método getter para la vista
-    public int getPuntos() {
-        return this.puntos;
-    }
     
     @Column(name = "direccion")
     private String direccion;
@@ -87,32 +72,26 @@ public class Usuario implements UserDetails {
     
 
     // Relación con Logros - Versión corregida
-    @ToString.Exclude
-    @JsonIgnore
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(
-        name = "usuario_logro",
-        joinColumns = @JoinColumn(name = "usuario_id"),
-        inverseJoinColumns = @JoinColumn(name = "logro_id")
-    )
-    private Set<Logro> logrosDesbloqueados = new HashSet<>();
-
 
     @OneToMany(mappedBy = "usuario")
     private Set<UsuarioLogro> usuarioLogros = new HashSet<>();
 
     
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-        name = "usuario_roles", 
-        joinColumns = @JoinColumn(name = "usuario_id"), 
-        inverseJoinColumns = @JoinColumn(name = "rol_id")
-    )
-    private Set<Rol> roles = new HashSet<>();
-
-
     // Métodos de UserDetails
+    
+ // Método para actualizar los puntos
+    public void actualizarPuntos() {
+        this.puntos = this.reciclajes.stream()
+                .mapToInt(Reciclaje::getPuntosGanados)
+                .sum();
+    }
+    
+    // Método getter para la vista
+    public int getPuntos() {
+        return this.puntos;
+    }
+    
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         Set<GrantedAuthority> authorities = new HashSet<>();
@@ -126,13 +105,14 @@ public class Usuario implements UserDetails {
     public String getUsername() {
         return email;
     }
-    
+
     public void setPuntos(int puntos){
     	
     	this.puntos = puntos;
     	actualizarPuntos();
     	
     }
+
 
     @Override
     public boolean isAccountNonExpired() {
@@ -154,15 +134,7 @@ public class Usuario implements UserDetails {
         return true;
     }
 
-    public void agregarLogro(Logro logro) {
-        this.logrosDesbloqueados.add(logro);
-        logro.getUsuarios().add(this);
-    }
 
-    public void eliminarLogro(Logro logro) {
-        this.logrosDesbloqueados.remove(logro);
-        logro.getUsuarios().remove(this);
-    }
 
    
 
